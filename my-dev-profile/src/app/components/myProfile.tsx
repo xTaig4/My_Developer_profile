00bf0e2441fb69f3a60d153b4abb01a7fd94712f
Taig4 <xTaig4@gmail.com>
2025-06-11 16:00:38 +0200

    color changes, view for smaller screen width

--- my-dev-profile/src/app/components/myProfile.tsx
+++ my-dev-profile/src/app/components/myProfile.tsx
@@ -7,20 +7,21 @@ interface MyProfileProps {
 }
 
 const MyProfile = ({ text }: MyProfileProps) => {
-  
   return (
-    <div className="flex flex-col-2 items-center pl-50  bg-gray-800 text-gray-200 gap-40 w-full h-screen">
-      <div className="flex flex-col text-left gap-4 w-130 mb-50">
-        <h1>My Profile</h1>
-        <p>{text}</p>
-        <EscapeButton />
-      </div>
+    <div
+      className="flex items-center flex-col pl-36 pr-5 pt-10 gap-5 bg-background text-gray-200 w-full h-screen
+                 md:flex-row md:pl-40 md:pb-19 md:gap-20"
+    >
       <div>
         <img
-          src="https://media.discordapp.net/attachments/668167761792466963/1316025712204972032/20241210_135217.jpg?ex=684193f4&is=68404274&hm=0f60b2aeaec6b64129479fb8e927d3897f55c7fbdf66d89886495a07503e2afe&=&format=webp&width=692&height=968"
+          src="https://media.discordapp.net/attachments/668167761792466963/1316025712204972032/20241210_135217.jpg?ex=6848d434&is=684782b4&hm=ec9edaaf93d38c14ed769a06b5ec13356971017f98a2d3f70538cb559e8590bf&=&format=webp&width=618&height=864"
           alt="My Profile Picture"
-          className="h-150 rounded-lg"
+          className="max-h-100 md:max-h-130 rounded-lg -scale-x-100 border-2 border-text"
         ></img>
+      </div>
+      <div className="flex text-left flex-col gap-3 max-w-130 mt-30 text-text">
+        <h1>My Profile</h1>
+        <p>{text}</p>
       </div>
     </div>
   );
