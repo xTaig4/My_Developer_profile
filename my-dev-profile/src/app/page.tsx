514528b4f05871163a64baeb43a833b4ee93f872
Taig4 <xTaig4@gmail.com>
2025-06-13 15:14:31 +0200

    Update page.tsx

--- my-dev-profile/src/app/page.tsx
+++ my-dev-profile/src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <div className="flex flex-col text-text items-start justify-center pb-25 min-h-screen text-left px-50 bg-background">
       <h1 className="text-8xl font-bold mb-4">
         Hi, <br />
-        I'm Tai, <br />a passionate developer!
+        I`m Tai, <br />a passionate developer!
       </h1>
       <p className="text-2xl max-w-4xl">
         Explore my developer profile to learn more about my skills, projects,
