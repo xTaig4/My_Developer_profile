bfabcdb632ef96224f63ea6a1eaadb5a4ddc3d91
Taig4 <xTaig4@gmail.com>
2025-06-17 15:29:32 +0200

    clean up

--- my-dev-profile/src/app/pages/Skills/page.tsx
+++ my-dev-profile/src/app/pages/Skills/page.tsx
@@ -12,10 +12,6 @@ const skills = [
 ];
 
 const SkillsPage = () => {
-  // Split skills into two columns after 4 skills
-  const firstColumn = skills.slice(0, 4);
-  const secondColumn = skills.slice(4);
-
   return (
     <div className="flex flex-col ml-54 items-center justify-center h-screen bg-background text-white">
       <div>
@@ -24,10 +20,8 @@ const SkillsPage = () => {
         </h2>
         <div className="grid grid-cols-1 lg:grid-cols-2 max-w-5xl gap-x-40 gap-y-5 text-text text-3xl">
           {skills.map((skill) => (
-            <li className="flex gap-10">
-              <span className="w-50" key={skill.name}>
-                {skill.name}
-              </span>
+            <li className="flex gap-10 items-center" key={skill.name}>
+              <span className="w-50">{skill.name}</span>
               <span>
                 {"★".repeat(skill.rating)}
                 {"☆".repeat(5 - skill.rating)}
@@ -39,44 +33,5 @@ const SkillsPage = () => {
     </div>
   );
 };
-
-function SkillsRating(
-  skill: { name: string; rating: number },
-  firstColumn: any[],
-  secondColumn: any[]
-) {
-  return (
-    <div>
-      <ul className="list-disc space-y-2">
-        {firstColumn.map((skill) => (
-          <li
-            key={skill.name}
-            className="flex items-center text-text text-lg gap-10"
-          >
-            <span className="w-32">{skill.name}</span>
-            <span className="text-4xl text-text">
-              {"★".repeat(skill.rating)}
-              {"☆".repeat(5 - skill.rating)}
-            </span>
-          </li>
-        ))}
-      </ul>
-      <ul className="list-disc ml-8 space-y-2">
-        {secondColumn.map((skill) => (
-          <li
-            key={skill.name}
-            className="flex items-center text-text text-lg gap-10"
-          >
-            <span className="w-32">{skill.name}</span>
-            <span className="ml-2 text-4xl text-text">
-              {"★".repeat(skill.rating)}
-              {"☆".repeat(5 - skill.rating)}
-            </span>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
 
 export default SkillsPage;
