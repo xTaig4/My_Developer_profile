b7fe50044783f60fa06c37ed345ec02509448eb4
Tai Nguyen <73821893+xTaig4@users.noreply.github.com>
2025-09-09 09:16:49 +0200

    reverting some changes to filter

--- my-dev-profile/src/app/pages/Profile/page.tsx
+++ my-dev-profile/src/app/pages/Profile/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+
 import Image from "next/image";
 import { useState } from "react";
 import SkillsBox from "@/app/components/SkillsBox";
@@ -44,60 +44,41 @@ const ProfilePage = () => {
 
   const [containerIndex, setContainerIndex] = useState(0);
 
-  const filters: string[] = ["none", "beard1.png", "master.png"];
+  const filters: string[] = ["", "beard1.png", "master.png"];
   const [filterIndex, setFilterIndex] = useState(0);
-  const [previousIndex, setPreviousIndex] = useState(0);
-  const [direction, setDirection] = useState<"next" | "prev">("prev");
-  const [isAnimating, setIsAnimating] = useState(false);
-
-  const changeFilter = (dir: "next" | "prev") => {
-    if (isAnimating) return;
-    setIsAnimating(true);
-    setDirection(dir);
-    setPreviousIndex(filterIndex);
-    setFilterIndex((prev) => {
-      if (dir === "next") {
-        return prev < filters.length - 1 ? prev + 1 : 0;
-      } else {
-        return prev > 0 ? prev - 1 : filters.length - 1;
-      }
-    });
-  };
-  const activeFilter = filters[filterIndex];
-  const previousFilter = filters[previousIndex];
 
   return (
     <div
-      className="flex flex-col items-center justify-center min-h-lvh"
+      className="flex min-h-lvh flex-col items-center justify-center"
       style={{ fontFamily: "pixelFont" }}
     >
-      <div className="flex-col lg:flex-row text-text p-4 flex 2xl:ml-40">
+      <div className="text-text flex flex-col p-4 lg:flex-row 2xl:ml-40">
         {/*RIGHT COLUMN: Projects*/}
-        <div className="flex gap-10 lg:flex-row flex-col-reverse items-center">
+        <div className="flex flex-col-reverse items-center gap-10 lg:flex-row">
           <ProjectList />
           <div className="flex flex-col">
             {/* TOP CONTAINER */}
-            <div className="flex flex-col-2 justify-evenly w-230">
+            <div className="flex-col-2 flex w-230 justify-evenly">
               {/* RIGHT COLUMN: Stats + Traits + Hobbies */}
-              <div className="flex flex-col gap-10 mt-10 h-75">
-                <section className="flex flex-col mt-5 justify-baseline">
+              <div className="mt-10 flex h-75 flex-col gap-10">
+                <section className="mt-5 flex flex-col justify-baseline">
                   <h1 className="text-4xl">Stats</h1>
                   {stats.map(({ stat, fillPercent }) => (
                     <div
                       key={stat}
-                      className="flex items-center justify-between gap-10 mt-4"
+                      className="mt-4 flex items-center justify-between gap-10"
                     >
                       <span className="flex text-lg">{stat}</span>
-                      <div className="flex w-50 h-5 bg-gray-900 rounded-full overflow-hidden">
+                      <div className="flex h-5 w-50 overflow-hidden rounded-full bg-gray-900">
                         <div
-                          className="flex h-full bg-text"
+                          className="bg-text flex h-full"
                           style={{ width: `${fillPercent}%` }}
                         ></div>
                       </div>
                     </div>
                   ))}
                 </section>
-                <div className="flex flex-col-2 gap-15">
+                <div className="flex-col-2 flex gap-15">
                   <section>
                     {/* Traits */}
                     <h1>Traits</h1>
@@ -111,22 +92,33 @@ const ProfilePage = () => {
                     <h1>Hobbies</h1>
                     <span>
                       {hobbies.map(({ hobby }) => (
-                        <li key={hobby}>{hobby}</li>))}
+                        <li key={hobby}>{hobby}</li>
+                      ))}
                     </span>
                   </section>
                 </div>
               </div>
               {/* LEFT COLUM: Profile image container */}
-              <div className="flex flex-col relative w-90 p-4 overflow-hidden">
-                <div className="flex justify-center mb-2">
+              <div className="relative flex w-90 flex-col overflow-hidden p-4">
+                <div className="mb-2 flex justify-center">
                   <button
-                    className="text-3xl flex w-10 h-7 items-center justify-center border-1 p-1 hover:bg-gray-600"
-                    onClick={() => changeFilter("prev")}>
+                    className="flex h-7 w-10 items-center justify-center border-1 p-1 text-3xl hover:bg-gray-600"
+                    onClick={() =>
+                      setFilterIndex((prev) =>
+                        prev > 0 ? prev - 1 : filters.length - 1,
+                      )
+                    }
+                  >
                     ←
                   </button>
                   <button
-                    className="text-3xl flex w-10 h-7 items-center justify-center border-1 p-1 hover:bg-gray-600"
-                    onClick={() => changeFilter("next")}>
+                    className="flex h-7 w-10 items-center justify-center border-1 p-1 text-3xl hover:bg-gray-600"
+                    onClick={() =>
+                      setFilterIndex((prev) =>
+                        prev < filters.length - 1 ? prev + 1 : 0,
+                      )
+                    }
+                  >
                     →
                   </button>
                 </div>
@@ -135,49 +127,16 @@ const ProfilePage = () => {
                   height={480}
                   src="/profile_pic.png"
                   alt="Profile"
-                  className="w-full object-cover border-1 border-text"
+                  className="border-text w-full border-1 object-cover"
                 />
-                {activeFilter !== "none" && (
-                  <div>
-                    <Image
-                      key={activeFilter}
-                      width={360}
-                      height={480}
-                      src={`/${activeFilter}`}
-                      alt="current overlay"
-                      className={`p-4 absolute w-90 object-cover
-                      ${direction === 'next' ? 'overlay-in-next' : 'overlay-in-prev'}`}
-                    />
-                    <Image
-                      key={previousFilter}
-                      width={360}
-                      height={480}
-                      src={`/${previousFilter}`}
-                      alt="previous overlay"
-                      className={`p-4 absolute w-90 object-cover
-                      ${isAnimating ? (direction === 'next' ? 'overlay-in-next' : 'overlay-in-prev') : ''}`}
-                    />
-
-                    {/* <Image
-                      key={activeFilter}
-                      width={360}
-                      height={480}
-                      src={`/${activeFilter}`}
-                      alt="current overlay"
-                      className="p-4 absolute w-90 object-cover
-                      ${direction === 'next' ? 'overlay-out-next' : 'overlay-out-prev'}
-                    />
+                {filters[filterIndex] !== "" && (
                   <Image
-                    key={previousFilter}
                     width={360}
                     height={480}
-                    src={`/${previousFilter}`}
-                    alt="previous overlay"
-                    className={`p-4 absolute w-90 object-cover
-                    ${isAnimating ? (direction === 'next' ? 'overlay-in-next' : 'overlay-in-prev') : ''}`}
-                    /> */}
-
-                  </div>
+                    src={`/${filters[filterIndex]}`}
+                    alt="current overlay"
+                    className={`absolute w-90 -translate-x-5 translate-y-6 object-cover p-4`}
+                  />
                 )}
               </div>
             </div>
@@ -186,23 +145,23 @@ const ProfilePage = () => {
               <button
                 onClick={() =>
                   setContainerIndex((prev) =>
-                    prev > 0 ? prev - 1 : containers.length - 1
+                    prev > 0 ? prev - 1 : containers.length - 1,
                   )
                 }
-                className="text-3xl left-150 flex w-10 h-7 items-center justify-center border-1 p-1 hover:bg-gray-600"
+                className="left-150 flex h-7 w-10 items-center justify-center border-1 p-1 text-3xl hover:bg-gray-600"
               >
                 ←
               </button>
-              <div className="flex gap-50 ">
+              <div className="flex gap-50">
                 {containers[containerIndex].content}
               </div>
               <button
                 onClick={() =>
                   setContainerIndex((prev) =>
-                    prev < containers.length - 1 ? prev + 1 : 0
+                    prev < containers.length - 1 ? prev + 1 : 0,
                   )
                 }
-                className="text-3xl right-96 flex w-10 h-7 items-center justify-center border-1 p-1 hover:bg-gray-600"
+                className="right-96 flex h-7 w-10 items-center justify-center border-1 p-1 text-3xl hover:bg-gray-600"
               >
                 →
               </button>
